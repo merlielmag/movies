1e94908586d5065b2d227c92312a2887d8c941a9
Rafelix <lomerli@gmail.com>
2017-05-26 12:32:49 +0200

    afegit taula top pelis

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526084253) do
+ActiveRecord::Schema.define(version: 20170526102912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,17 @@ ActiveRecord::Schema.define(version: 20170526084253) do
     t.integer  "year"
   end
 
+  create_table "top_movies", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "stars"
+    t.string   "name"
+    t.integer  "year"
+    t.integer  "ranking"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_top_movies_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -49,4 +60,5 @@ ActiveRecord::Schema.define(version: 20170526084253) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "top_movies", "users"
 end
