9efd508b06a3513bfbfc6db891e0a2c241a1cd3e
Rafelix <lomerli@gmail.com>
2017-05-26 10:08:39 +0200

    afegim controlador statics

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'statics/index'
+
+  get 'statics/about'
+
   root 'peliculas#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
