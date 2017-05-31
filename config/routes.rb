542055b94c89fc600962b49b834b29c493118516
Rafelix <lomerli@gmail.com>
2017-05-31 19:20:28 +0200

    afegit resources :peliculas a routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
 
   root 'statics#index'
 
+  resources :peliculas
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
