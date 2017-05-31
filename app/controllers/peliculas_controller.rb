6558d2ff94ecf25fe8c1d396802fe0227d7bf69f
Rafelix <lomerli@gmail.com>
2017-05-31 21:49:33 +0200

    afegit visualitzar les pelis al index

--- app/controllers/peliculas_controller.rb
+++ app/controllers/peliculas_controller.rb
@@ -1,4 +1,24 @@
 class PeliculasController < ApplicationController
   def index
+    @pelicula = Pelicula.all
   end
+
+  def show
+    @pelicula = Pelicula.find(params[:id])
+  end
+
+  def new
+    @pelicula = Pelicula.new
+  end
+
+  def create
+    @pelicula = Pelicula.new(peliculas_params)
+
+    if @pelicula.save
+      redirect_to peliculas_path, notice: "Se ha guardado la peli!!"
+    else
+      render :new
+    end
+  end
+
 end
