60fb6892bcc41528d117e0ea5122b9ed8c4ad7e8
Rafelix <lomerli@gmail.com>
2017-06-16 03:11:18 +0200

    afegit AdminUser admin@admin.com i no_admin@admin.com

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
     def initialize(user)
         user ||= AdminUser.new # guest user (not logged in)
 
-        if user.email == 'x@x.com'
+        if user.email == 'admin@admin.com'
           can :manage, :all
         else
           can :read, :all
