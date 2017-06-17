ab12621f8b3bcbdde919d35e35a9bdebd8e7b6ea
Rafelix <lomerli@gmail.com>
2017-06-17 16:10:37 +0200

    creat model payment i afegits camps a la BDD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616074039) do
+ActiveRecord::Schema.define(version: 20170617140845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,18 @@ ActiveRecord::Schema.define(version: 20170616074039) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "payments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.boolean  "subscription"
+    t.string   "channel"
+    t.boolean  "active"
+    t.integer  "plan"
+    t.bigint   "amount"
+    t.index ["user_id"], name: "index_payments_on_user_id", using: :btree
+  end
+
   create_table "peliculas", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -121,6 +133,7 @@ ActiveRecord::Schema.define(version: 20170616074039) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "payments", "users"
   add_foreign_key "settings", "users"
   add_foreign_key "top_movies", "users"
 end
