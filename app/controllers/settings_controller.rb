d1cd661f303c9b336e11b9c01e865d4053674be5
Rafelix <lomerli@gmail.com>
2017-06-19 19:27:03 +0200

    pulit el settings_controller

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -7,7 +7,6 @@ class SettingsController < ApplicationController
   def index
     @user = current_user
     @setting = current_user.setting
-    # @payments = Payment.all
     @payment = Payment.where(user_id: @user.id).all
   end
 
