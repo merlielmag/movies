00444df956e2a9fadc97001a57bddc9e71caafb6
Rafelix <lomerli@gmail.com>
2017-06-20 22:29:15 +0200

    pulit error marketing mailer

--- app/mailers/marketing_mailer.rb
+++ app/mailers/marketing_mailer.rb
@@ -4,21 +4,22 @@ class MarketingMailer < ActionMailer::Base
   default :from => "support@merliapps.com"
   default :content_type => "text/html"
 
-def onboarding_mailer(user)
+  def onboarding_mailer(user)
 
-  @user = user
-  @email = user.email
+    @user = user
+    @email = user.email
 
-  I18n.with_locale(@user.locale) do
-    mail from: I18n.t('mailer.maria'),
-    to: @email,
-    bcc: "youthcandoit@kolau.com",
-    subject: I18n.t('mailer.el_titulo_html', first_name: @user.first_name)
+    I18n.with_locale(@user.locale) do
+      mail from: I18n.t('mailer.maria'),
+      to: @email,
+      bcc: "youthcandoit@kolau.com",
+      subject: I18n.t('mailer.el_titulo_html', first_name: @user.first_name)
+    end
   end
-end
 
-def onboarding_mailer_second_mailer
-end
+  def onboarding_mailer_second_mailer
+  end
 
-def onboarding_mailer_third_mailer
+  def onboarding_mailer_third_mailer
+  end
 end
