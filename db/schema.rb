3ad6015dd05fd96ef1af8ccf070e698a995ebe91
Rafelix <lomerli@gmail.com>
2017-06-20 10:25:51 +0200

    afegida taula log_managers pels emails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170617140845) do
+ActiveRecord::Schema.define(version: 20170620075406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,12 @@ ActiveRecord::Schema.define(version: 20170617140845) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "log_managers", force: :cascade do |t|
+    t.integer "user"
+    t.text    "exception_message"
+    t.string  "tag"
   end
 
   create_table "payments", force: :cascade do |t|
