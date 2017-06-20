c838c52b19c3716e4d9c548781b59781f568c203
Rafelix <lomerli@gmail.com>
2017-06-20 23:03:00 +0200

    deliver_now

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -14,7 +14,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     super
     if @user.persisted?
-      Marketing::OnboardingMailer.set(wait: 1.hour).perform_later(@user)
+      # Marketing::OnboardingMailer.set(wait: 1.hour).perform_later(@user)
+      Marketing::OnboardingMailer.deliver_now(@user)
     end
   end
 
