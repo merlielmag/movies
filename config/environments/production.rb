5e7136c3b4f198219a80840cf19fbaf5e77e78a9
Rafelix <lomerli@gmail.com>
2017-06-20 23:44:09 +0200

    perform_now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
+    domain: ENV["DOMAIN"],
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["MAILER_USERNAME"],
